6763b2aa6e9b64f0a21ee40e66a7a7d135bc0c40
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-01-24 16:06:58 +0530

    Form tried

---
+++ form.js
@@ -0,0 +1,32 @@
+function check()
+{
+    let adhaarno=document.getElementById('adhaar').value;
+    let voterid=document.getElementById('voter').value;
+    let rationcard=document.getElementById('ration').value;
+
+
+    let user={
+        adhaar:adhaarno,
+        voter:voterid,
+        ration:rationcard
+    }
+
+
+    if(adhaarno.length==12)
+    {
+        fetch('https://form-78d91-default-rtdb.firebaseio.com/user.json',
+        {
+            method:'post',
+            body:JSON.stringify(user)
+        }).then(()=>
+        {
+            alert('Your Data is saved successfully.');
+            document.getElementById('adhaar').value="";
+            document.getElementById('voter').value="";
+            document.getElementById('ration').value="";
+        })      
+    }
+    else{
+        alert("Please fill all Details Properly.")
+    }
+}
